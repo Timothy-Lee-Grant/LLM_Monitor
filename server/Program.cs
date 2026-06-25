831bf9a7d3a130cc45fd82ce2044bfbbdbac37fb
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-06-25 00:05:48 -0700

    adding middleware scaffold

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddReverseProxy()
+//     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Work that can write to the response. (1)");
+    await next.Invoke(context);
+    Console.WriteLine("Work that doesn't write to the response. (1)");
+});
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Work that can write to the response. (2)");
+    await next.Invoke(context);
+    Console.WriteLine("Work that doesn't write to the response. (2)");
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello world!");
+});
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("This statement isn't reached. (3)");
+    await next.Invoke(context);
+    Console.WriteLine("This statement isn't reached. (3)");
+});
 
 app.Run();
