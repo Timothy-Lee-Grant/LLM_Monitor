b03af308df155671d06c478eca2f469385925499
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-07-02 18:13:59 -0700

    Starting to seperate instructions to tell ollama service to pull model.

---
+++ langchain_service/app/models/Instructions.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+# All this function should do is ensure that the user's desired model is successfully pulled / downloaded inside the ollama container.
+def TryGetOllamaModel(desired_model:str, base_url:str) -> bool:
+    pass
--- langchain_service/app/models/factory.py
+++ langchain_service/app/models/factory.py
@@ -30,20 +30,11 @@ class ModelFactory:
         if os.getenv("LLM_MODE") == "mock":
             return MockChatModel
         
-        # don't waste networking if model is already known to be in my ollama
-        # Oh no! is it that because this is static I can't access the dictionary??
-        # if userDesiredModel in self.knownPulledModels:
-        #     return ChatOllama(model=userDesiredModel, temperature=0)
-
-        # I need to first check if this model is already loaded by ollama. (check pull?)
-        # I don't want to do a lot of network calls every time the user asks a message. Therefore I should implement a dictionary which determines only once per system start that ollama does have the model pulled
-
         # I need to do a POST request to my ollama service (if not in mock mode)
 
         # Not 'localhost' but I am still shaking on docker networking
         base_url = os.getenv("OLLAMA_BASE_URL", "http://ollama:11434")
         try:
-            # Question: When I print, where does this actually go? It is in a docker container, so is it to my logs? What is the connection to the C# idea of _logger?
             print(f"Checking if model {userDesiredModel} is locally available in my ollama service docker container.")
             response = requests.get(f"{base_url}/api/tags", timeout=10)
             response.raise_for_status()
@@ -57,14 +48,27 @@ class ModelFactory:
             # standardize tag check: if no tag given, chekc both raw name and ':latest'
             has_model = (userDesiredModel in downloaded_names) or (f"{userDesiredModel}:latest" in downloaded_names)
 
-            if has_model:
-                print("model was found")
-                return .
-            
+            if not has_model:
+                # Now we need to send a POST request to our ollama docker container telling it to pull the requested user's model
+                # TODO: In the future I will need to secure this so that only 'accepted' models are allowed to be passed in by the user
+                print("Model was NOT found in ollama")
+                print("Pulling Model Now")
+                payload = {
+                    "model":userDesiredModel,
+                    "stream": False     #setting stream to false will cause this to wait until ollama has finished fully pulling the model
+                }
+
+                response = requests.post(f"{base_url}/api/pull", json=payload, timeout=None) # timeout none to avoid timing out for networking when we are pulling and download a large model.
+
+                if response.json().get("status") != "success":
+                    print("Unable to download model")
+                    assert(False)
 
         except requests.RequestException as e:
             print(f"Error communicating with the ollama service container during get_chat_model creation")
             return None
+        
+        #
 
 
     @staticmethod
