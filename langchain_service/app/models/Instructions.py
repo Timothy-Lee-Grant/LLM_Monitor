89ced0765993d8229b0f2e303e02a9f47aec3d74
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-07-02 18:20:00 -0700

    Feeling iffy on implementation of InstructOllama function, but will move forward and look at it later.

--- langchain_service/app/models/Instructions.py
+++ langchain_service/app/models/Instructions.py
@@ -2,10 +2,45 @@
 
 
 
-
+import requests
 
 
 
 # All this function should do is ensure that the user's desired model is successfully pulled / downloaded inside the ollama container.
 def TryGetOllamaModel(desired_model:str, base_url:str) -> bool:
-    pass
+    
+    try:
+        print(f"Checking if model {desired_model} is locally available in my ollama service docker container.")
+        response = requests.get(f"{base_url}/api/tags", timeout=10)
+        response.raise_for_status()
+
+        #parse the local model list which ollama service gave back to me
+        local_models = response.json().get("models", [])
+
+        # look through downloaded tags (ollama uses exact name matching or maps to :latest)
+        downloaded_names = [m["name"] for m in local_models]
+        
+        # standardize tag check: if no tag given, chekc both raw name and ':latest'
+        has_model = (desired_model in downloaded_names) or (f"{desired_model}:latest" in downloaded_names)
+
+        if not has_model:
+            # Now we need to send a POST request to our ollama docker container telling it to pull the requested user's model
+            # TODO: In the future I will need to secure this so that only 'accepted' models are allowed to be passed in by the user
+            print("Model was NOT found in ollama")
+            print("Pulling Model Now")
+            payload = {
+                "model":desired_model,
+                "stream": False     #setting stream to false will cause this to wait until ollama has finished fully pulling the model
+            }
+
+            response = requests.post(f"{base_url}/api/pull", json=payload, timeout=None) # timeout none to avoid timing out for networking when we are pulling and download a large model.
+
+            if response.json().get("status") != "success":
+                print("Unable to download model")
+                return False
+        
+        return True
+
+    except requests.RequestException as e:
+        print(f"Error communicating with the ollama service container during get_chat_model creation")
+        return False
--- langchain_service/app/models/factory.py
+++ langchain_service/app/models/factory.py
@@ -1,10 +1,10 @@
 import os
-import requests
+
 from langchain_ollama import ChatOllama
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.outputs import ChatResult, ChatGeneration
 from langchain_core.messages import AIMessage
-
+from app.models.Instructions import TryGetOllamaModel
 
 
 
@@ -30,45 +30,14 @@ class ModelFactory:
         if os.getenv("LLM_MODE") == "mock":
             return MockChatModel
         
-        # I need to do a POST request to my ollama service (if not in mock mode)
-
-        # Not 'localhost' but I am still shaking on docker networking
         base_url = os.getenv("OLLAMA_BASE_URL", "http://ollama:11434")
-        try:
-            print(f"Checking if model {userDesiredModel} is locally available in my ollama service docker container.")
-            response = requests.get(f"{base_url}/api/tags", timeout=10)
-            response.raise_for_status()
+        res = TryGetOllamaModel(userDesiredModel, base_url)
 
-            #parse the local model list which ollama service gave back to me
-            local_models = response.json().get("models", [])
+        if not res:
+            # There was an issue durring the instruction to get ollama service to pull the model
+            return None # Or maybe we return a mock?
 
-            # look through downloaded tags (ollama uses exact name matching or maps to :latest)
-            downloaded_names = [m["name"] for m in local_models]
-            
-            # standardize tag check: if no tag given, chekc both raw name and ':latest'
-            has_model = (userDesiredModel in downloaded_names) or (f"{userDesiredModel}:latest" in downloaded_names)
 
-            if not has_model:
-                # Now we need to send a POST request to our ollama docker container telling it to pull the requested user's model
-                # TODO: In the future I will need to secure this so that only 'accepted' models are allowed to be passed in by the user
-                print("Model was NOT found in ollama")
-                print("Pulling Model Now")
-                payload = {
-                    "model":userDesiredModel,
-                    "stream": False     #setting stream to false will cause this to wait until ollama has finished fully pulling the model
-                }
-
-                response = requests.post(f"{base_url}/api/pull", json=payload, timeout=None) # timeout none to avoid timing out for networking when we are pulling and download a large model.
-
-                if response.json().get("status") != "success":
-                    print("Unable to download model")
-                    assert(False)
-
-        except requests.RequestException as e:
-            print(f"Error communicating with the ollama service container during get_chat_model creation")
-            return None
-        
-        #
 
 
     @staticmethod
